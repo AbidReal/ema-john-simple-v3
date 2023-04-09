f4a7f79e3ad7960d843d824f5385986e5727db63
Abid Hasan <108365863+AbidReal@users.noreply.github.com>
2023-04-09 22:01:48 +0600

    review-items del-btn working

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import "./ReviewItem.css";
 
-const ReviewItem = ({ product }) => {
+const ReviewItem = ({ product, handleRemoveFromCart }) => {
   const { img, name, price, id, quantity } = product;
   return (
     <div className="review-item">
@@ -14,10 +14,10 @@ const ReviewItem = ({ product }) => {
           Price: <span className="orange-text">${price}</span>
         </p>
         <p>
-          Order Quantity: <span className="orange-text">${quantity}</span>
+          Order Quantity: <span className="orange-text">{quantity}</span>
         </p>
       </div>
-      <button className="btn-delete">
+      <button onClick={() => handleRemoveFromCart(id)} className="btn-delete">
         <FontAwesomeIcon className="delete-icon" icon={faTrashAlt} />
       </button>
     </div>
