a7de3fccb909ec93fe39dda720253a2e71ce2ce1
Abid Hasan <108365863+AbidReal@users.noreply.github.com>
2023-04-09 22:16:02 +0600

    clear cart done

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import { removeFromDb } from "../../utilities/fakedb";
+import { deleteShoppingCart, removeFromDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import ReviewItem from "../ReviewItem/ReviewItem";
 import "./Orders.css";
@@ -12,6 +12,11 @@ const Orders = () => {
     const remaining = cart.filter((product) => product.id !== id);
     setCart(remaining);
     removeFromDb(id);
+  };
+
+  const handleClearCart = () => {
+    setCart([]);
+    deleteShoppingCart();
   };
   return (
     <div className="shop-container">
@@ -25,7 +30,7 @@ const Orders = () => {
         ))}
       </div>
       <div className="cart-container">
-        <Cart cart={cart}></Cart>
+        <Cart cart={cart} handleClearCart={handleClearCart}></Cart>
       </div>
     </div>
   );
